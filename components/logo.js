cb48d23b35c17a36cf8733ca88983f76f89f3612
Duy Anh Nguyen <shuoppaii@gmail.com>
2022-02-27 15:26:03 -0500

    change to logo

--- components/logo.js
+++ components/logo.js
@@ -14,21 +14,22 @@ const LogoBox = styled.span`
 
   img {
     transition: 200ms ease;
+    
   }
 
   &:hover img {
-    transform: rotate(20deg);
+    transform: translateY(-2px);
   }
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/rocket.png`
+  const footPrintImg = `/images/whitehouseicon.png`
 
   return (
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Image src={footPrintImg} width={20} height={20} alt="logo"  />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
